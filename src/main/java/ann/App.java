be91d503d29cf62d79a8ff00b8c9eb555c350e55
Evgeny Cherkashin <eugene@irnok.net>
2017-04-01 19:56:50 +0800

    Try to create a package.

---
+++ src/main/java/ann/App.java
@@ -0,0 +1,13 @@
+package ru.icc.cells.lod.ann;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
