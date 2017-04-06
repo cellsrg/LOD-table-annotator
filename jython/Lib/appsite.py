f65bbc204a9f5c5bdf04904b1412a1141b6373b1
Evgeny Cherkashin <eugene@irnok.net>
2017-04-06 22:19:08 +0800

    Now we can develop cool apps.

--- jython/Lib/appsite.py
+++ jython/Lib/appsite.py
@@ -6,11 +6,15 @@ import os, os.path
 mainpath=sys.path.pop(0)
 mainpath=os.path.abspath(mainpath)
 site_packages = os.path.join(mainpath,"site-packages")
+app_packages = os.path.join(mainpath,"app-packages")
 sys.path.insert(0,site_packages)
-sys.path.insert(0,mainpath)
+sys.path.insert(0,app_packages)  # a higher priority
+sys.path.insert(0,mainpath)      # .. even higher
 
 # Make jython somewhat compartible to CPython-3.X
 
 from six import *
 
 # place here Your code and imports
+
+import main
