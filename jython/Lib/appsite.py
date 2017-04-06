66a9beff1e0ca179290554c4835cbaceee12b09c
Evgeny Cherkashin <eugene@irnok.net>
2017-04-06 22:07:27 +0800

    Added six package.

--- jython/Lib/appsite.py
+++ jython/Lib/appsite.py
@@ -1,1 +1,16 @@
-from __future__ import print_function
+import sys
+import os, os.path
+
+# Install default path for modules
+
+mainpath=sys.path.pop(0)
+mainpath=os.path.abspath(mainpath)
+site_packages = os.path.join(mainpath,"site-packages")
+sys.path.insert(0,site_packages)
+sys.path.insert(0,mainpath)
+
+# Make jython somewhat compartible to CPython-3.X
+
+from six import *
+
+# place here Your code and imports
