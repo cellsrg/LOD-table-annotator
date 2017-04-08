3601e3dfdb6e2c10074e9c3d7a1c4e73b6f0aa67
Evgeny Cherkashin <eugene@irnok.net>
2017-04-08 08:53:55 +0800

    Tried to deal with RDFa and JSON-LD but filed.

--- jython/Lib/app-packages/jena.py
+++ jython/Lib/app-packages/jena.py
@@ -3,20 +3,38 @@ from six import *
 from java.lang import System
 
 from org.apache.jena.rdf.model import Model
+from org.apache.jena.rdf.model import ModelFactory
 from org.apache.jena.rdf.model import RDFNode
 from org.apache.jena.rdf.model import Resource
 from org.apache.jena.rdf.model import Statement
 from org.apache.jena.rdf.model import StmtIterator
 from org.apache.jena.util import FileManager
+# from net.rootdev.javardfa.jena import RDFaReader
+from org.apache.jena.query import Query
+
+from java.lang import Class
+
 import os.path
 
-data = os.path.abspath("data/data.ttl")
+# data = os.path.abspath("data/data.ttl")
+data = os.path.abspath("data/document.ttl")
+# data = os.path.abspath("data/document.jsonld")
+# data = os.path.abspath("data/document.xhtml")
 
-model = FileManager.get().loadModel(data, None, "TURTLE");
-it = model.listStatements();
+# Class.forName("net.rootdev.javardfa.jena.RDFaReader")
+model = FileManager.get().loadModel(data)  # , None, "TURTLE")
+# model = ModelFactory.createDefaultModel()
+# model.read(data, "TURTLE")
 
-for stmt in it:
-    s,p,o = stmt.getSubject(), stmt.getPredicate(), stmt.getObject()
-    print(s,p,o)
+print(System.out)
+
+
+def print_graph(model):
+    for stmt in model.listStatements():
+        # print(stmt)   # This works too, next two lines
+        # could be commented out
+        s, p, o = stmt.getSubject(), stmt.getPredicate(), stmt.getObject()
+        print(s, p, o)
+
 
 model.write(System.out, "TURTLE")
